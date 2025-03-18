79652d21a6ba3a638665df5bd94190509c8e69db
Jorge-Luis008 <jluis.alvarado888@gmail.com>
2025-03-18 12:39:02 -0700

    Nombre de municipio
    + Se añadio una funcion para buscar el nombre del municipio segun el estado
     > Actualmente no funciona bien para los estados de la pagina 1

--- scripts/script.js
+++ scripts/script.js
@@ -62,30 +62,35 @@ async function checkButton(){
     var stateNum = stateInput.value;
     var muniNum = municipalityInput.value;
 
+    //BUSCAR EL NOMBRE DEL ESTADO
+    //Variables a cambiar
     var data2 = 0;
     var stateLength = 0;
     var url2 = [0];
     var idStart = 0;
-debugger
-    if (stateNum<16){
+
+    //Buscar la pagina que contenga el estado correcto
+    if (stateNum<=15){
         url2 = ['https://sepomex.icalialabs.com/api/v1/states?page=1'];
-        idStart = 0;
+        idStart = -1;
     } else if(stateNum>=16 && stateNum<=30){
         url2 = ['https://sepomex.icalialabs.com/api/v1/states?page=2'];
         idStart = 16;
     } else if(stateNum>30){
         url2 = ['https://sepomex.icalialabs.com/api/v1/states?page=3'];
-        idStart = 30;
+        idStart = 31;
     }
 
+    //Obtener el numero de estados en la pagina
     const stateName = await fetch(url2).then(response => response.json()).then(data => {
         data2 = data
         stateLength = data.states.length
     })
 
+    //Funcion para buscar el estado correcto y su nombre
     let x = 0;
     while (x < stateLength){
-        if (idStart == stateNum - 1){
+        if (idStart == stateNum ){
             var state2 = data2.states[x].name;
             break;
         }
@@ -93,6 +98,34 @@ debugger
         idStart = idStart + 1;
     }
 
+    //BUSCAR EL NOMBRE DEL MUNICIPIO
+    //Variables a cambiar
+    var data3 = 0;
+    var muniLength = 0;
+    var idBegin = 0;
+
+    //Buscar la pagina del estado con sus municipios
+    var selState2 = stateList.value;
+    const url3 = ['https://sepomex.icalialabs.com/api/v1/states/'+selState2+'/municipalities'];
+
+    //Obtener el numero de municipios en la pagina
+    const muniNumb = await fetch(url3).then(response2 => response2.json()).then(data => {
+        data3 = data;
+        muniLength = data.municipalities.length;
+    })
+
+    //Funcion para buscar el municipio correcto y su nombre
+    let y = 0;
+    while (y < muniLength){
+        if (idBegin == muniNum ){
+            var muni2 = data3.municipalities[y].name;
+            break
+        }
+        y = y + 1;
+        idBegin = idBegin + 1;
+    }
+
+    //Funcion para validar los campos
     if (validarCampos()){
         const bodyRequest = {
             nombre: nameInput1.value,
@@ -101,7 +134,7 @@ debugger
             calle: streetInput.value,
             codigoPostal: zipCodeInput.value,
             estado: state2,
-            municipio: municipalityInput
+            municipio: muni2
         };
 
         const url2 = "https://postman-echo.com/post";
